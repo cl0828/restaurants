21459415ec0b1187f0fa94c1ad147c020285131a
cl0828 <>
2018-11-27 16:14:58 +0800

    Add api_key function

--- routes/api.js
+++ routes/api.js
@@ -3,28 +3,32 @@ var router = express.Router();
 var multer = require('multer');
 var multerUpload = multer();
 var db = require('../database');
-var authGuard = require('./middlewares/authguard');
 
-router.post('/restaurant/create', authGuard, multerUpload.single('photo'), function (req, res, next) {
+router.post('/restaurant/create', multerUpload.single('photo'), function (req, res, next) {
   // server-side validation
-  if (!req.body.name) {
+  if (!req.body.name || !req.body.api_key) {
     return res.json({
       'status': 'failed'
     });
   }
-  const restaurant = req.body;
-  restaurant.owner = req.session.username;
-  if (req.file) {
-    restaurant.photo = {};
-    restaurant.photo.base64 = req.file.buffer.toString('base64');
-    restaurant.photo.mimetype = req.file.mimetype;
-  }
-  db.insert('restaurants', restaurant)
-    .then(result => {
-      res.json({
-        'status': 'ok',
-        '_id': result.insertedId
-      });
+  db.find('users', { 'api_key': req.body.api_key })
+    .then(docs => {
+      delete req.body.api_key;
+      const user = docs[0];
+      const restaurant = req.body;
+      restaurant.owner = user.username;
+      if (req.file) {
+        restaurant.photo = {};
+        restaurant.photo.base64 = req.file.buffer.toString('base64');
+        restaurant.photo.mimetype = req.file.mimetype;
+      }
+      db.insert('restaurants', restaurant)
+        .then(result => {
+          res.json({
+            'status': 'ok',
+            '_id': result.insertedId
+          });
+        });
     })
     .catch(err => {
       res.json({
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const uuidv4 = require('uuid/v4');
+var authGuard = require('./middlewares/authguard');
 var db = require('../database')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -37,6 +39,7 @@ router.get('/signup', function (req, res, next) {
 
 router.post('/signup', function (req, res, next) {
   // TODO: handle existed username
+  req.body.api_key = uuidv4();
   db.insert('users', req.body).then(result => {
     res.send('signed up!');
   }).catch(err => next(err));
@@ -53,6 +56,12 @@ router.get('/map', function (req, res, next) {
   res.render('map', {
     lat: req.query.lat, lon: req.query.lon, zoom: req.query.zoom
   });
+});
+
+router.get('/api_key', authGuard, function (req, res, next) {
+  db.find('users', { 'username': req.session.username })
+    .then(docs => res.render('api_key', { title: 'API Key', 'api_key': docs[0].api_key }))
+    .catch(err => next(err));
 });
 
 module.exports = router;
